7a55664aef3214e513392ee34fc85535e5750a08
Habibul Rahman <habib2211998.slg@gmail.com>
2022-07-18 14:26:03 +0530

    redesigning

---
+++ board.js
@@ -0,0 +1,17 @@
+const icon_human = document.querySelector(".fa-people-group");
+const icon_computer = document.querySelector(".fa-laptop-code");
+
+/*
+1. Play & Pause
+2. Whose turn it is ??
+3. Same status message box: Playing | Paused | X Wins | O Wins
+4. Lit up icon Human or Computer depending on who wins
+5. Check for Draw match
+6. Check for Winner
+7. Restart game
+8. Timer
+9. X-O Selection Box
+	 If the user selects he/her icons before the start of after the start of the 
+	 game then it will have the same effect as restarting the game button has,
+	 but this time the user will have the icon which he selects.
+*/
